9dc6b98d519317ec4cc34b47c58afeb4c7f30ce3
Alex Silcock <alex.silcock@skybettingandgaming.com>
2019-03-25 19:03:29 +0000

    Updated Bitbucket repo creation script to handle API changes
    Bitbucket are deprecating usernames in their API to avoid issues around
    PII and GDPR. As a result, usernames in URLs are now replaced with the
    Atlassian account ID.

--- scripts/bbqs.sh
+++ scripts/bbqs.sh
@@ -2,7 +2,8 @@
 
 # Simple script to create a BitBucket repo for the current project, and set it as remote
 
-username=asilcock
+username="asilcock"
+accid="557058:6c6fb8a9-49bb-4255-9623-29653757c799"
 
 echo 'Creates a BitBucket repo for a project, and sets the git remote'
 
@@ -12,7 +13,7 @@ read -p 'Password: ' -s password  # -s flag hides password text
 echo -e "\nCreating repo..."
 
 curl -X POST -u $username:$password -H "Content-Type: application/json" \
-  https://api.bitbucket.org/2.0/repositories/$username/$reponame \
+  https://api.bitbucket.org/2.0/repositories/$accid/$reponame \
   -d '{"scm": "git", "is_private": "true", "fork_policy": "no_public_forks" }'
 
 echo -e "\n"
